26abb214a4de6c5c5c3497be83e38d7ee5a5ef61
Rohan Poudel <rohzanpoudel@gmail.com>
2023-02-15 10:18:15 +0545

    Problem2 solved

---
+++ assign_2/Problem2.py
@@ -0,0 +1,20 @@
+def eveodd(num):
+    if num % 2 == 0 or num == 0:
+        print("Is even")
+    else:
+        print("Is odd")
+    
+#Test cases
+print("Testing eveodd(3): ", end="")
+eveodd(3)
+print()
+
+print("Testing eveodd(3): ", end="")
+eveodd(0)
+print()
+
+
+print("Testing eveodd(3): ", end="")
+eveodd(-2)
+
+    
