f647e0b6cdd2cfa18c7d0fdeb8a33d1f5b554cec
Rohan Poudel <rohzanpoudel@gmail.com>
2023-02-15 10:18:47 +0545

    Problem3 solved

---
+++ assign_2/Problem3.py
@@ -0,0 +1,25 @@
+def waterBill(gall):
+    price = 50
+    if gall <= 110:
+        print(price)
+    elif gall > 110:
+        extra = gall - 110
+        price = float(price) + (extra * 0.75)
+        print(price)
+
+#Test cases
+print("Testing waterBill(60): ", end="")
+waterBill(60)
+print()
+
+print("Testing waterBill(110): ", end="")
+waterBill(110)
+print()
+
+print("Testing waterBill(115): ", end="")
+waterBill(115)
+print()
+
+print("Testing waterBill(145): ", end="")
+waterBill(145)
+
